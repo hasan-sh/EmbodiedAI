334be01244a26bda55f169b7b64713ecdf2a47d1
Hasan_sh <hasansh343@gmail.com>
2020-06-09 10:16:01 +0200

    symmetric sites

--- experiments/aggregation/parameters.py
+++ experiments/aggregation/parameters.py
@@ -1,15 +1,16 @@
-def experiment0(screensize): # Single aggregation site
-    area_loc1 = [screensize[0 ] /2., screensize[1] /2.]
+def experiment0(screensize):  # Single aggregation site
+    area_loc1 = [screensize[0] / 2., screensize[1] / 2.]
 
-    scale1 = [110, 110] # assuming a 1000 by 1000 screen
+    scale1 = [110, 110]  # assuming a 1000 by 1000 screen
 
     big = False
 
     return area_loc1, scale1, big
+
+
 """
 Parameter settings to be loaded in the model
 """
-
 """
 General settings (DO NOT CHANGE)
 """
@@ -19,52 +20,43 @@ SCREEN = (S_WIDTH, S_HEIGHT)
 
 #choose how long to run the simulation
 #-1 : infinite, N: finite
-FRAMES=-1
+FRAMES = -1
 
 #choose swarm type
 SWARM = 'Aggregation'
 #define the number of agents
 N_AGENTS = 40
 #object location
-OBJECT_LOC = [S_WIDTH/2., S_HEIGHT/2.]
+OBJECT_LOC = [S_WIDTH / 2., S_HEIGHT / 2.]
 
 CONVEX = True
 
-
 #Agent Settings:
 #agent size
-WIDTH=10
-HEIGHT=8
+WIDTH = 10
+HEIGHT = 8
 #update
-dT=0.2
+dT = 0.2
 #agents mass
-MASS=20
+MASS = 20
 #agent maximum/minimum speed
 MAX_SPEED = 7.
 MIN_SPEED = 4.
 
-
 #Boid Settings:
 #velocity force
 MAX_FORCE = 8.
-
 """
 Simulation settings to adjust:
 """
-
 """
 Aggregation class parameters (defines the environment of where the aggregation to act)
 """
 #Define the environment
-SYMMETRIC_SITES = False
+SYMMETRIC_SITES = True
 SITES_DIFFER = True
-
-
 """
 Boid class parameters
 """
 #view of site and other agents
-RADIUS_VIEW=100
-
-
-
+RADIUS_VIEW = 100
