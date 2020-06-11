90cf75daaf851e39bffeeb74c0ef37d00220cd6c
Hasan_sh <hasansh343@gmail.com>
2020-06-11 16:47:57 +0100

    Basic and trivial probability for leaving a site

--- experiments/aggregation/cockroach.py
+++ experiments/aggregation/cockroach.py
@@ -39,8 +39,8 @@ class Cockroach(Agent):
                 # print(random.random())
                 # self._state.append(p.JOINING)
             else:
-                self.change_state(p.WANDERING)
-        self.site_behavior();
+                self.change_state(self._state[-1])
+        self.site_behavior()
 
     def site_behavior(self):
         # join or leave with a certain prob.
@@ -48,38 +48,65 @@ class Cockroach(Agent):
         pass
 
     def change_state(self, state):
-        p_state = self._state.pop()
+        c_state = self._state.pop()
+        p_state = None
+        if bool(self._state):
+            p_state = self._state.pop()
         if state == p.WANDERING:
             # if agent is joining, let it be
             if p_state == p.JOINING:
                 self._state.append(p_state)
-                pass
+            elif p_state == p.LEAVING:
+                self._state.append(p_state)
+                self._state.append(state)
             else:
                 self._state.append(state)
-
         elif state == p.STILL:
             # calculate the prob. of staying/leaving and change the state
-            self._state.append(state)
-        elif state == p.JOINING:
+            pl = self.leave()
+            if pl <= 0.2:
+                self._state.append(state)
+                self._state.append(p.LEAVING)
+            else:
+                self._state.append(c_state)
+        elif state == p.JOINING and p_state != p.LEAVING:
             in_center = pygame.sprite.spritecollide(
                 self, self.aggregation.objects.sites, False,
-                pygame.sprite.collide_circle_ratio(0.7))
-            if bool(in_center):
-                self.v = numpy.zeros(2)
-                self._state.append(p.STILL)
+                pygame.sprite.collide_mask)#collide_circle_ratio(0.7))
+            if bool(in_center):# and prev_state != p.WANDERING:
+                # the following is just to make them stop smoothly
+                self.dT -= 0.007
+                if self.dT <= 0.01:
+                    self.dT = 0
+                    self._state.append(state)
+                    self._state.append(p.STILL)
+                else:
+                    self._state.append(c_state)
             else:
-                self._state.append(state)
+                self._state.append(c_state)
         else:
-            print('leaving')
+            # make agent move again
+            self.dT = p.dT
             self._state.append(state)
-    #         if bool(collide):
-    #             self.avoid_obstacle(obstacle.pos, self.flock.object_loc)
-
-    #     align_force, cohesion_force, separate_force = self.neighbor_forces()
-
-    #     #combine the vectors in one
-    #     steering_force = align_force * p.ALIGNMENT_WEIGHT  + cohesion_force * p.COHESION_WEIGHT + separate_force * p.SEPARATION_WEIGHT
-
-    #     #adjust the direction of the boid
-    #     self.steering += helperfunctions.truncate(steering_force / self.mass, p.MAX_FORCE)
-    #     self.steering *= random.randint(2, 4) if min(steering_force) < 0 else .8
+            self._state.append(p.WANDERING)
+    
+    def leave(self):
+        i_s = self.agent_in_sites()
+        # the follwoing gets all sites, maybe, in some way, 
+        # we should include the size of the current site into the probablity.
+        # for s in self.aggregation.objects.sites:
+        #     print('pos', s.rect)
+        len_is = len(i_s)
+        # the following isn't right; agent only knows within a radius!
+        p = len_is / self.aggregation.n_agents
+        # the next is a prob. within the site 
+        # p = random.randint(1, len_is - 2 if len_is > 3 else 2) / len(i_s) 
+        # awaiting a better model!
+        return p
+    
+    def agent_in_sites(self):
+        result = []
+        for a in self.aggregation.agents:
+            result += pygame.sprite.spritecollide(a, self.aggregation.objects.sites, False, pygame.sprite.collide_circle)
+        return result
+    
