d79fa0eb3b35cf2b8840ed2b7df253529efaf729
Sara Prifti <sara.prifti@pxlwidgets.com>
2020-06-15 17:38:40 +0200

    edited Person.py

--- experiments/covid/person.py
+++ experiments/covid/person.py
@@ -10,11 +10,10 @@ class Person(Agent):
     def __init__(self,
                  pos,
                  velocity,
-                 population,
-                 image=pygame.draw.circle(p.SCREEN, color=(255, 153, 18), center=(200, 200), radius=20)):
+                 population):
         super(Person, self).__init__(pos,
                                      velocity,
-                                     image,
+                                     color=(255, 153, 18),  # orange
                                      max_speed=p.MAX_SPEED,
                                      min_speed=p.MIN_SPEED,
                                      mass=p.MASS,
