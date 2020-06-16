536d568419fa18fccbe4dce1fd75bbeae4d51600
Hasan_sh <hasansh343@gmail.com>
2020-06-16 15:28:34 +0200

    basic implementation of SIR

--- experiments/covid/person.py
+++ experiments/covid/person.py
@@ -26,33 +26,60 @@ class Person(Agent):
                                      height=p.HEIGHT,
                                      dT=p.dT)
         self.population = population
+        self.p_infection = self.calc_prob_infection()  # find prob of infection
+        self.infected_at = None
+        self.clock = pygame.time.Clock()
+        self.count = 0
 
     def update_actions(self):
-        print(self._state)
+        # print(self._state)
         # avoid any obstacles in the environment
         for obstacle in self.population.objects.obstacles:
             collide = pygame.sprite.collide_mask(self, obstacle)
             if bool(collide):
                 self.avoid_obstacle()
 
-        p_infection = self.calc_prob_infection()  # find prob of infection
-        start_infection = 0.0
+        if self._state == p.RECOVERED:
+            # since agent recovered, no need for further calculation
+            # this if statement is just for fun!!
+            if self.dT == p.dT:
+                self.dT *= 0.2
+            return
+        self.p_infection = self.calc_prob_infection()  # find prob of infection
+        # TODO: change 0.4 to a parameter; this should be based on a differential equation
+        if self._state == p.SUSCEPTIBLE and self.p_infection >= 0.4:
+            # TODO: change the radius to a parameter
+            all_neighbors = self.population.find_neighbors(self, radius=25)
+            neighbors_in_radius = len(all_neighbors) >= 1
+            if neighbors_in_radius:
+                self._state = p.INFECTIOUS
+                self._color = p.RED
+                if not self.infected_at:
+                    self.infected_at = self.clock.tick()
+        if self._state == p.INFECTIOUS:
+                #start_infection) >= 55:  # 55seconds before getting recovered
+            # start_infection = self.calc_infected_time()
+            self.count += 1
+            # TODO: change the time for an agent to recover based on the recovery rate;
+            # recovery and infection rates should sum up to 1; i.e. probability
+            # the time can then be calculated as 1 / r
+            # print('start', start_infection)
+            # if start_infection >= 5:
+            if self.count >= 500:
+                self._state = p.RECOVERED
+                self._color = p.GREEN
+                self.infected_at = None
+        self.update_color()
 
-        if self._state == p.SUSCEPTIBLE and p_infection >= 0.4 and len(
-                self.population.find_neighbors(self, radius=25)) >= 1:
-            self._state = p.INFECTIOUS
-            self.color = p.RED
-            start_infection = pygame.time.get_ticks()
-            # print("infected")
+    def update_color(self):
+        self.image.fill(self._color)
 
-        elif self._state == p.INFECTIOUS and self.calc_infected_time(
-                start_infection) >= 55:  # 55seconds before getting recovered
-            self._state = p.RECOVERED
-            self.color = p.GREEN
-            # print("recovered")
 
     def calc_prob_infection(self):
+        # TODO: the rate could/should be based on a parameter; easier for farther calculations!
         return round(random.uniform(0.3, 0.8), 1)
 
-    def calc_infected_time(self, start_time):
-        return (pygame.time.get_ticks() - start_time) / 1000
+    def calc_infected_time(self):
+        t = self.clock.tick()
+        # print(t)
+        return t / 1000
